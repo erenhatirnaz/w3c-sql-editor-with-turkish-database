e091c5defd138814e9cd1182c99d848703c831e5
ErenHatirnaz <erenhatirnaz@hotmail.com.tr>
2015-01-23 02:10:23 +0200

    updated 2 test and added new  1 test and messages were added to all assertions.
    testIsCreatedDatabaseFile: changed assertion method to assertFileExists.
    renamed: testIsDatabaseCreatedSuccessfully ->testIsTablesCreatedSuccessfully
    testIsTablesCreatedSuccessfully: changed assertion method to assertEmpty.
    added testIsNotEmptyTables.

--- tests/RandomDatabaseGeneratorTest.php
+++ tests/RandomDatabaseGeneratorTest.php
@@ -21,28 +21,43 @@ class RandomDatabaseGeneratorTest extends PHPUnit_Framework_TestCase {
         $this->generator->generateDatabase();
 
         $this->db = new PDO("sqlite:database.db");
-        $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_COLUMN);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     /**
      * @test
      */
     public function testIsCreatedDatabaseFile() {
-        $this->assertTrue(file_exists('database.db'));
+        $this->assertFileExists('database.db', "The database could not be created!");
     }
 
     /**
      * @test
      */
-    public function testIsDatabaseCreatedSuccessfully(){
-        $databaseTables = array('Calisanlar', 'KargoFirmalari', 'Kategoriler', 'Musteriler', 'Saticilar', 'Urunler', 'Siparisler', 'SiparisDetaylari');
+    public function testIsTablesCreatedSuccessfully() {
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_COLUMN);
+        $databaseTables = array( 'Calisanlar', 'KargoFirmalari', 'Kategoriler', 'Musteriler', 'Saticilar', 'Urunler', 'Siparisler', 'SiparisDetaylari' );
 
-        $getTableList = $this->db->prepare("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence'");
-        $getTableList->execute();
-        $tableNames = $getTableList->fetchAll();
+        $getTableNames = $this->db->prepare("SELECT name FROM sqlite_sequence ORDER BY name");
+        $getTableNames->execute();
+        $tableNames = $getTableNames->fetchAll();
 
-        $this->assertEquals($databaseTables, $tableNames);
+        $this->assertEmpty(array_merge(array_diff($databaseTables, $tableNames), array_diff($tableNames, $databaseTables)), "All tables are not created!");
+    }
+
+    /**
+     * @test
+     */
+    public function testIsNotEmptyTables() {
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_NAMED);
+
+        $getTables = $this->db->prepare("SELECT * FROM sqlite_sequence");
+        $getTables->execute();
+        $tables = $getTables->fetchAll();
+
+        foreach ($tables as $table) {
+            $this->assertNotEquals(0, intval($table["seq"]), $table["name"] . " is empty!");
+        }
     }
 
     private function deleteDatabaseFile() {
